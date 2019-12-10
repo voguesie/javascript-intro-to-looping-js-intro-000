a632d6b63f1aa10ef675fbf0e6b76a40e4eed968
Michael Vogel <voguesie@alumni.stanford.edu>
2019-12-10 03:03:44 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 // forLoop takes an array as an input, and adds strings with a for loop.
 
-var foorLoop = function (loopArray) {
+function forLoop(loopArray) {
   for (var i = 1; i <= 25; i++) {
     if i === 1 {
       loopArray.push("I am 1 strange loop.");
